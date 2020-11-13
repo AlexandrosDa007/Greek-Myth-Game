f97594ebca14e3a993762fddd2df07c9714b10eb
Alex Daltsis <alexdaltsis@gmail.com>
2020-11-13 23:24:06 +0200

    Adds dice animation

---
+++ Assets/DiceSide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSide : MonoBehaviour
+{
+    bool onGround;
+    public int sideValue;
+
+    public bool OnGround { get => onGround; set => onGround = value; }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "ground")
+        {
+            OnGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "ground")
+        {
+            OnGround = false;
+        }
+    }
+}
---
+++ Assets/RealDice.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealDice : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public bool hasLanded;
+    public bool thrown;
+
+    private Vector3 initialPosition;
+
+    public int diceValue;
+
+    public DiceSide[] diceSides;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RollDice();
+        }
+
+        if (rb.IsSleeping() && !hasLanded && thrown)
+        {
+            hasLanded = true;
+            rb.useGravity = false;
+            SideValueCheck();
+
+        }
+        else if (rb.IsSleeping() && hasLanded && diceValue == 0)
+        {
+            RollAgain();
+        }
+    }
+
+    void RollDice()
+    {
+        if (!thrown && !hasLanded)
+        {
+            thrown = true;
+            rb.useGravity = true;
+            rb.AddTorque(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500));
+        }
+        else if (thrown && hasLanded)
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        transform.position = initialPosition;
+        thrown = false;
+        hasLanded = false;
+        rb.useGravity = false;
+
+    }
+
+    void RollAgain()
+    {
+        Reset();
+        thrown = true;
+        rb.useGravity = true;
+        rb.AddTorque(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500));
+
+    }
+
+    void SideValueCheck()
+    {
+        diceValue = 0;
+        
+        foreach (DiceSide side in diceSides)
+        {
+            
+            if (side.OnGround)
+            {
+                diceValue = side.sideValue;
+                Debug.Log("You got: diceValue");
+            }
+        }
+
+    }
+}
